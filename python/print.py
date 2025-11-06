d74beaddbf9b7e587c741069cd1c038df2d27212
Beant Grewal <beantsingh.grewal@hotmail.com>
2025-11-06 13:19:49 -0600

    verified map function for single argument

--- python/print.py
+++ python/print.py
@@ -1,15 +1,18 @@
 # split function for multline user input
 print("hey there !")
+n =int(input("Enter a number: "))
+print(n)
+"""
 x,y,z = map(int, input("Set the x,y,z coordinates ").split())
 
-"""
 x = int(x)
 y = int(y)
 z = int(z)
-"""
 
 print("x : ", x)
 print("y : ", y)
 print("z : ", z)
 print(x+y)
 print(x*y)
+
+"""
